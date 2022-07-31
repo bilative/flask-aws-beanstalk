5b67291b832b001db95479af7cd236c2f32801a5
bilative <bilallozdemir@outlook.com>
2022-07-31 04:37:00 +0300

    example code

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import Blueprint, render_template
+import numpy as np
+import pandas as pd
+import pickle
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
